952d08975a8d3bd50866fa2e86a9f62f132b6111
charushi <turkarcs@rknec.edu>
2020-11-02 23:17:12 +0530

    Lab Assignment4

---
+++ Desktop/Lab Assignments/Lab Assignment4/BankAccount/src/BankAccount/Person.java
@@ -0,0 +1,49 @@
+package BankAccount;
+
+/**
+ * author: Charushi
+ * date :31/10/2020
+ * desc :Person class contains name and age of a person
+ */
+
+public class Person {
+
+	/**
+	 * declaring the variables
+	 */
+          private String name;
+          private float age;
+          /**
+           * parameterised constructor
+           * @param name
+           * @param age
+           */
+
+          public Person(String name,float age) {
+        	  super();
+        	  this.name=name;
+        	  this.age=age;
+          }
+          /**
+           * getters and setters
+           * @return
+           */
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public float getAge() {
+			return age;
+		}
+
+		public void setAge(float age) {
+			this.age = age;
+		}
+          
+	
+
+}
